1a05bf16a7afe13b61b06354bf5469b58589c07e
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2022-02-20 21:59:14 +0530

    image slider created
    create image slider and link js

--- Home page/js/style.js
+++ Home page/js/style.js
@@ -39,3 +39,12 @@ btns.forEach((btn,i) => {
     });
 
 });
+  //image slider
+  var counter = 1;
+  setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+      counter = 1;
+    }
+  }, 5000);
