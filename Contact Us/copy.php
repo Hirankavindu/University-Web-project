1e420b14eb7002eaa739eedd65b2a613c9e45450
VNimasha <vishminimasha394@gmail.com>
2022-02-24 00:55:50 +0530

    css err2

--- Contact Us/copy.php
+++ Contact Us/copy.php
@@ -7,7 +7,7 @@
       $receiver = "edu4u2022@gmail.com";
       $subject = $message;
       $sender = $email;
-      
+ 
       if 
       (mail($receiver, $subject, $sender))
       { 
@@ -18,11 +18,11 @@
       }
 
       $to= "edu4u2022@gmail.com";
-     
+      
   }
- ?>
-    <!DOCTYPE html>
-    <html lang="en"> 
+?>
+  <!DOCTYPE html>
+  <html lang="en"> 
     <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
@@ -31,95 +31,124 @@
        <title>Document</title>
        <link rel="stylesheet" href="copy.css">
        <link rel="stylesheet" href="cpy.css">
-    
     </head>
-
+    
     <body>
      <div class="container">
  <header>
-       <h1>Contact Us</h1><br>
+       <h1>Contact Us!</h1><br>
           <p2>
         <i>
-          </h2>
+          </h2 1>
             We shall only collect personal information that the users voluntarily provide for processing their requests and shall not use such information for any other purpose. Such personal information may be disclosed only as required by law.<br>
             Send us your comments and suggestions via e-mail.<br>
             .............contact us!.................
-          </h2>
+          </h2 1>
         </i>
     </p2>
-    </header>     
-    </body>
-</header>
-<div class="content">
-    <div class="content-form">
-        <section>
-            <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i> 
-            <h2>Address</h2>
-            <p>
-                Pitipana<br>
-                Thalagala Rd<br>
-                Homagama.
-            </p>
-         </section>
-       
-         <section>
-             <i class="fa fa-phone fa-2x" aria hidden="true"></i>
-             <h2>Phone</h2>
-             <p>076-6313771</p>
-         </section>
+ </header>
+       <div class="content">
+        <div class="content-form">
+            <section>
+                <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i> 
+                <h2>Address</h2>
+                <p>
+                    Pitipana<br>
+                    Thalagala Rd<br>
+                    Homagama.
+                </p>
+             </section>
+           
+             <section>
+                 <i class="fa fa-phone fa-2x" aria hidden="true"></i>
+                 <h2>Phone</h2>
+                 <p>076-6313771</p>
+             </section>
 
-         <section>
-             <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
-             <h2>E-mail</h2>
-             <p>
-                 Edu4Uonlinelibrary@gmail.com
-             </p>
-         </section>    
-         <div class="container">      
-   </div>
-</div>  
+             <section>
+                 <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                 <h2>E-mail</h2>
+                 <p>
+                     Edu4Uonlinelibrary@gmail.com
+                 </p>
+             </section>          
+       </div>
 
-     <form>
-         <div class="form"ction="" method="post" autocomplete="off">
-         <div class="form">
-         <div class="right">
-               <div class="contact-form">
-                   <input type="text" name="name" class="input" required>
-                     <span><b>Full name</b></span>
+       <div class="container">
+
+       </div>
+    </div>  
+    
+         <form action="" method="post" autocomplete="off">
+             <div class="form">
+               <div class="right">
+                   <div class="contact-form">
+                       <input type="text" name="name" class="input" required/>
+                         <span><b> Full name </b></span>
+                   </div>
+                   <div class="contact-form">
+                       <input type="E-mail" name="email" class="input" required/>
+                       <label for="">E-mail</label>
+                       <span><b>E-mail</b></span>
+                   </div>
+                   <div class="contact-form">
+                       <textarea name="message" name="message" class="input"></textarea>
+                       <label for="">Type Your Text Here......</label>
+                        <span><b>
+                            Type Your Text Here......
+                        </b></span>
+
+                    </div>
+                    <div class="contact-form">
+                       <input type="submit"name="submit">
+                    </div>
+
                </div>
-               <div class="contact-form">
-                   <input type="E-mail" name="email" class="input" required>
-                   <span><b>E-mail</b></span>
-               </div>
+             </div>
 
-               <div class="contact-form">
-                   <textarea name="message" name="message" class="input"></textarea>
-                   <span><b>
-                        Type Your Text Here......
-                    </b></span>
-                </div>
-                <div class="contact-form">
-                   <input type="submit"name="submit">
-                </div>
+         </form>
+         <div class="media">
+             <li><i class="fa fa-facebook-square fa-2x"aria-hidden="true"></i> </li>
+             <li><i class="fa fa-instagram fa-2x"aria-hidden="true"></i></li>
+             <li><i class="fa fa-whatsapp fa-2x"aria-hidden="true"></i></li>
+             <li><i class="fa fa-twitter-square fa-2x"aria-hidden="true"></i></li>
+             <li><i class="fa fa-linkedin fa-2x"></i></li>
 
-           </div>
          </div>
 
-     </form>
-     <div class="media">
-         <li><i class="fa fa-facebook-square fa-2x"aria-hidden="true"></i> </li>
-         <li><i class="fa fa-instagram fa-2x"aria-hidden="true"></i></li>
-         <li><i class="fa fa-whatsapp fa-2x"aria-hidden="true"></i></li>
-         <li><i class="fa fa-twitter-square fa-2x"aria-hidden="true"></i></li>
-         <li><i class="fa fa-linkedin fa-2x"></i></li>
+         <div class="empty">
 
-     </div>
-
-     <div class="empty">
-
-     </div>
- </div>  
-</body>    
+         </div>
+     </div>      
+                
+            
+         
+     
+   </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
